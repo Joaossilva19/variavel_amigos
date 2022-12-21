6419b221a1af8c52becf92a5cf769d02610cb500
Joaossilva19 <117737134+Joaossilva19@users.noreply.github.com>
2022-12-21 22:58:53 +0000

    Add files via upload

---
+++ variável.js
@@ -0,0 +1,41 @@
+let name = "João";
+ console.log(nome)
+let age = 19;
+ console.log(19)
+ console.log(19*7)
+let students =[
+    {
+        name: "Cunha",
+        age: 19,
+        isStudent: true
+    },
+    {
+        name: "Cátia",
+        age: 19,
+        isStudante: false
+    },
+    {
+        name: "Letícia",
+        age: 19,
+        isStudante: true
+    },
+]
+console.log(students);
+//João
+
+let caract =[
+    {
+        name: "Cunha",
+        caract: "Português/ Bicicleta/ telemóvel",
+    },
+    {
+        name: "Cátia",
+        caract: "Portuguesa/ Vizela/ pequena",
+    },
+    {
+        name: "Leticia",
+        caract: "Portuguesa/ Aldeia/ namorado",
+    },
+
+]
+console.log(caract)
